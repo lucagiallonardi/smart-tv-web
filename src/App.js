f46d868249423f2da15bd2569709b522f632cdf1
Luca Giallonardi <40302955+lucagiallonardi@users.noreply.github.com>
2024-01-23 16:56:59 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,12 +28,12 @@ function App() {
       <>
       <Router>
       <Routes>
-        <Route path="/login" element={(<Login handleLogin={handleLogin}/>)}></Route>
         <Route path="/" element={authenticated ? (
               <Home handleLogout={handleLogout} />
             ) : (
             <Login handleLogin={handleLogin}/>)}></Route>
-       <Route path="/register" element={<Register/>}></Route>
+       <Route path="/register" element={authenticated?<Home handleLogout={handleLogout} /> : (<Register/>)}></Route>
+      <Route path="/login" element={(<Login handleLogin={handleLogin}/>)}></Route>
        <Route path="/store" element={authenticated?<Store/> : (<Login handleLogin={handleLogin}/>)}></Route>
        <Route path="/favorites" element={authenticated? <Favorites/> : (<Login handleLogin={handleLogin}/>)}></Route>       
       </Routes>
