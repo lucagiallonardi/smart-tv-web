5d422965610a592d135c5dc145a3ace79817110a
Luca Giallonardi <40302955+lucagiallonardi@users.noreply.github.com>
2024-01-23 16:29:44 -0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
             try{
                 const decodedToken = jwt_decode(token);
                 const userId = decodedToken.sub;
-                const response = await fetch('https://gtv-render.onrender.com/home/api/items',{
+                const response = await fetch('https://gtv-render.onrender.com/api/items',{
                     headers:{
                         Authorization: `${token}`,
                         'User-Id': userId,
@@ -88,7 +88,7 @@ const Home = () => {
         // agregar favoritos
         const addFavoriteItem = async (newItem) => {
           try {
-            const response = await fetch('https://gtv-render.onrender.com/home/api/add-favorite-item', {
+            const response = await fetch('https://gtv-render.onrender.com/api/add-favorite-item', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
@@ -122,7 +122,7 @@ const Home = () => {
         const addItemFavoritePlaceholder = { title: "Agregar", image: "https://i.postimg.cc/W33cWb9R/simbolo.jpg", link: "/" };
         const fetchItemsFromServer = async () => {
     try {
-      const response = await fetch('https://gtv-render.onrender.com/home/api/favorite-items', {
+      const response = await fetch('https://gtv-render.onrender.com/api/favorite-items', {
         headers: {
           Authorization: token,
         },
@@ -247,7 +247,7 @@ if(isLoading){
                 {groupedItems2.map((group, index) => (
                   <div className={`carousel-item recomended-item ${index === 0 ? "active" : ""}`} key={index}>
                     {group.map((item, itemIndex) => (
-                      <Link to={item.link}><img src={item.image} alt={item.title} key={item._id || itemIndex} 
+                      <Link to={item.link}><img src={item.image} alt={item.title} target="_blank" key={item._id || itemIndex} 
                       className={item.title === "Agregar" ? "simboloMas recomended-item-img" : "recomended-item-img"} onClick={() => {
                         if (item.title === "Agregar") {
                           setShowForm(true);
